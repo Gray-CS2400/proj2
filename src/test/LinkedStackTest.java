236a5241a9159d805acc115029a67d7619723aa8
maxlaucpp <152106962+maxlaucpp@users.noreply.github.com>
2024-03-16 15:58:09 -0700

    Update LinkedStackTest.java

--- src/test/LinkedStackTest.java
+++ src/test/LinkedStackTest.java
@@ -1,2 +1,10 @@
-public class LinkedStackTest {
+//class LinkedStackTest
+class LinkedStackTest {
+    //main method
+    public static void main (String[] args) throws Exception {
+        String infix = "a*b/(c-a)+d*e";
+        //call the convertToPostfix method
+        String postfix = convertToPostfix(infix);
+        System.out.println ("Postfix expression: " + postfix);
+    }
 }
