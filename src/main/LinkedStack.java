9161fc84bdcd04e6d60269c65bf5f21fc19606d9
maxlaucpp <152106962+maxlaucpp@users.noreply.github.com>
2024-03-16 18:59:11 -0700

    Update LinkedStack.java
    Made backup file for old version just incase of issues.

--- src/main/LinkedStack.java
+++ src/main/LinkedStack.java
@@ -1,57 +1,77 @@
 package main;
+import java.util.*;
 
-import java.util.EmptyStackException;
-/**
-    A class of stacks whose entries are stored in a chain of nodes.
-    @author Frank M. Carrano and Timothy M. Henry
-    @version 5.0
-*/
-public final class LinkedStack<T> implements StackInterface<T>
+public class LinkedStack
 {
-	private Node topNode; // References the first node in the chain
-  
-   public LinkedStack()
-   {
-      topNode = null;
-   } // end default constructor
-  
-//  < Implementations of the stack operations go here. >
-//  . . .
-
-	private class Node
+	
+	static int order(char a)
 	{
-      private T    data; // Entry in stack
-      private Node next; // Link to next node
-      
-      private Node(T dataPortion)
-      {
-         this(dataPortion, null);
-      } // end constructor
-      
-      private Node(T dataPortion, Node linkPortion)
-      {
-         data = dataPortion;
-         next = linkPortion;
-      } // end constructor
-      
-      private T getData()
-      {
-         return data;
-      } // end getData
-      
-      private void setData(T newData)
-      {
-         data = newData;
-      } // end setData
-      
-      private Node getNextNode()
-      {
-         return next;
-      } // end getNextNode
-      
-      private void setNextNode(Node nextNode)
-      {
-         next = nextNode;
-      } // end setNextNode
-	} // end Node
-} // end LinkedStack
+		switch (a)  //creating switch for priority deciding
+		{
+		case '+':
+		case '-':
+			return 1;
+	
+		case '*':
+		case '/':
+			return 2;
+	
+		case '^':
+			return 3;
+		}
+		return -1;
+	}
+	
+
+	static String conversion(String a) //creating cinversion function
+	{
+
+		String res = new String("");//blank string is created
+		
+		Stack<Character> ss = new Stack<>();//stack is created
+		
+		for (int i = 0; i<a.length(); ++i) //iterarting
+		{
+			char c = a.charAt(i);//choosing character of string one by one
+			
+			if (Character.isLetterOrDigit(c))//if it is operand it will simply add to string
+				res += c;
+		
+			else if (c == '(')
+				ss.push(c); // if it is left paranthesis it will simply push into stack
+			
+			else if (c == ')') //if right paranthesis then character will add string upto left paranthesis not meet or stack becomes empty
+			{
+				while (!ss.isEmpty() &&
+						ss.peek() != '(')
+					res += ss.pop();
+				
+					ss.pop();
+			}
+			else 
+			{
+				while (!ss.isEmpty() && order(c)   //if it is token then on basis of priority  it goes on stack only when lowest priotiy tokens will there otherwise pop them
+						<= order(ss.peek())){
+					
+					res += ss.pop();
+			}
+				ss.push(c);
+			}
+	
+		}
+	
+		while (!ss.isEmpty()){
+			if(ss.peek() == '('){   // if we found that left paranthesis it means there is no right paranthesis would exist hence it is invalid
+				return "invalid data given";
+			}
+			res += ss.pop(); //otherwise finally popping element and adding it to res
+		}
+		return res;//finally returning res
+	}
+
+	public static void main(String[] args)
+	{
+		String z ="a*b-(c+d)+e";
+		System.out.println(conversion(z));
+	}
+}
